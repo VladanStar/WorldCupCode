9880cf8a67e5dd60c76c3eee52e374ec3eb47193
unknown <vladancupric@gmail.com>
2022-04-08 07:45:58 +0200

    New commits

---
+++ classes/data.js
@@ -0,0 +1,8 @@
+export const groupsNames = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+export const worldTeams = [
+    ['Katar (1)'], ['Ekvador(2)'], ['Senegal(3)'], ['Holandija(4)'], ['Engleska(5)'], ['Iran(6)'], ['SAD(7)'], ['Ukrajina(8)'],  
+    ['Argentina(9)'], ['Saudijska Arabija(10)'], ['Meksiko(11)'], ['Poljska(12)'],[ 'Francuska(13)'], ['Peru(14)'], ['Danska(15)'], ['Tunis(16)'],
+   ['Spanija(17)'], ['Novi Zeland(18)'], ['Nemacka(19)'], ['Japan(20)'], ['Belgija(21)'], ['Kanada(22)'], ['Maroko(23)'], ['Hrvatska(24)'],
+    ['Brazil(25)'], ['Srbija(26)'], ['Svajcarska(27)'], ['Kamerun(28)'], ['Portugal(29)'], ['Gana(30)'],[ 'Urugvaj(31)'],[ 'Juzna Koreja(32)']
+];
