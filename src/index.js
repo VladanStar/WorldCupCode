376ec5c8868fe690ef6ed267d2906b1e5752f7b2
unknown <vladancupric@gmail.com>
2022-04-08 08:30:30 +0200

    New commits

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { nameWorldCup, config } from "./config.js";
+import {
+  worldTeams,
+  titlesPlayOff,
+  groupsNames,
+  JORNADA,
+  LOCAL_TEAM,
+  VISIT_TEAM,
+} from "../UI/data.js";
+import WorldCupSimulator from "../group/GroupPhase.js"
+import PlayOff from "../group/PlayOff.js";
