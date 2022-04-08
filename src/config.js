710d7a09ff90f6a02dbcda92ab29febcc1def5ac
unknown <vladancupric@gmail.com>
2022-04-08 08:25:42 +0200

    New commits

---
+++ src/config.js
@@ -0,0 +1,6 @@
+/* CONFIGURATION DATA */
+export const nameWorldCup = "WORLD CUP";
+export const config = { rounds: 1, 
+                        pointsPerWin: 3,
+                        pointsPerDrawn: 1,
+                        pointsPerLost: 0 };
