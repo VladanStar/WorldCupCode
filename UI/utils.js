e2253e3ef9030a49adb1bc007fd2eca50457683a
unknown <vladancupric@gmail.com>
2022-04-08 07:56:07 +0200

    New commits

--- UI/utils.js
+++ UI/utils.js
@@ -1,1 +1,11 @@
 import {worldTeams} from "./data.js"
+
+export const generateResultGoals = function() {
+
+    for (var i = 0; i< worldTeams.length;i++){
+   
+  
+      return Math.floor((Math.random() * (7)));
+    }
+    }
+     // console.log(generateResultGoals());
