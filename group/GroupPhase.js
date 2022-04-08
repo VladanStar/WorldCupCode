90468ef16cf1fe7060789fafd98b70c54046f417
unknown <vladancupric@gmail.com>
2022-04-08 08:20:28 +0200

    New commits

--- group/GroupPhase.js
+++ group/GroupPhase.js
@@ -1,3 +1,17 @@
 import { firstElementArray, groupsNames, secondElementArray } from "../UI/data.js";
 import Group from "./Group.js";
 import Team from "./Teams.js";
+
+export default class GroupPhase {
+
+    constructor(name, teams = [], config = {}) {
+        this.name = name;
+        this.teams = [];
+        this.config = {};
+        this.teamsPerGroup = teams.length / groupsNames.length;
+        this.groups = [];
+        this.setup(teams, config);
+        this.winnersToPlayOff = [];
+      }
+
+}
